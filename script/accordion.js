f294770ae91a104412678260d2d4a8033f431136
taishiasami123 <taishi.asami123@gmail.com>
2020-02-28 16:35:36 +0900

    アコーディオン用のscriptを追加

---
+++ script/accordion.js
@@ -0,0 +1,44 @@
+const check1 = document.getElementById("check1");
+const text1 = document.getElementById("text1");
+const arrow1 = document.getElementById("arrow1");
+
+check1.onclick = () => {
+    text1.classList.toggle("open");
+    arrow1.classList.toggle("open");
+};
+
+const check2 = document.getElementById("check2");
+const text2 = document.getElementById("text2");
+const arrow2 = document.getElementById("arrow2");
+
+check2.onclick = () => {
+    text2.classList.toggle("open");
+    arrow2.classList.toggle("open");
+};
+
+const check3 = document.getElementById("check3");
+const text3 = document.getElementById("text3");
+const arrow3 = document.getElementById("arrow3");
+
+check3.onclick = () => {
+    text3.classList.toggle("open");
+    arrow3.classList.toggle("open");
+};
+
+const check4 = document.getElementById("check4");
+const text4 = document.getElementById("text4");
+const arrow4 = document.getElementById("arrow4");
+
+check4.onclick = () => {
+    text4.classList.toggle("open");
+    arrow4.classList.toggle("open");
+};
+
+const check5 = document.getElementById("check5");
+const text5 = document.getElementById("text5");
+const arrow5 = document.getElementById("arrow5");
+
+check5.onclick = () => {
+    text5.classList.toggle("open");
+    arrow5.classList.toggle("open");
+};
