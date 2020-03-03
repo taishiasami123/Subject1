5103461bbf45c56ae6a3d8fd768e25fe4d0146f7
taishiasami123 <taishi.asami123@gmail.com>
2020-03-03 19:23:55 +0900

    ハンバーガーメニューに開閉するアイテムを追加

--- script/humberger.js
+++ script/humberger.js
@@ -1,10 +1,12 @@
 const humberger = document.getElementById("humberger");
 const line = document.getElementsByClassName("line");
+const li = document.getElementsByClassName("header-nav-list-li")
 const item = document.getElementsByClassName("header-nav-list-item");
 
 humberger.onclick = () => {
     for (let i = 0; i < line.length; i++) {
         line[i].classList.toggle("active");
+        li[i].classList.toggle("open")
         item[i].classList.toggle("open");
     };
 };
